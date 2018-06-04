c9e5eb9097b44056ef5d5c48cc9bf93b9c4de60f
Arseniy Perchik <login@axxeny.com>
2018-06-04 16:41:28 +0500

    Remove unneseccary usings.

--- MatrixMultiplierExam/FileProcessor.cs
+++ MatrixMultiplierExam/FileProcessor.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using AxxenyUtilities.Helpers;
 
 namespace MatrixMultiplierExam
